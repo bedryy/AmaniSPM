c89591696136560b5ce4cfd40f9eeb378db6b12e
bedryy <bedri@amani.tech>
2024-05-02 01:10:57 +0300

    added binary target and using path

--- Package.swift
+++ Package.swift
@@ -4,23 +4,31 @@
 import PackageDescription
 
 let package = Package(
-    name: "AmaniSPM",
+    name: "AmaniSDK",
+    platforms: [
+        .iOS(.v13) // Platform olarak iOS 13 ve üzerini destekliyoruz
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "AmaniSPM",
-            targets: ["AmaniSPM"]),
+            name: "AmaniSDK",
+            targets: ["AmaniSDK"]
+        )
     ],
     dependencies: [
-//        .package(url: "https://github.com/AMANI-AI-ORG/IOS.SDK.UI.git", from: "1.0.0")
+//        .package(url: "https://github.com/openssl/openssl.git", from: "3.1.5003") // OpenSSL bağımlılığı eklendi
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "AmaniSPM"),
-        .testTarget(
-            name: "AmaniSPMTests",
-            dependencies: ["AmaniSPM"]),
+            name: "AmaniSDK", // AmaniSDK hedefi
+            dependencies: [
+                .product(name: "OpenSSL", package: "OpenSSL") // OpenSSL ürününe bağımlılık eklendi
+            ],
+            path: "Users/bbedry/Desktop/AmaniAi/PublishAmaniSPM/PublishAmaniSPM"
+        ),
+        .binaryTarget(
+            name: "AmaniSDKBinary", // Binary hedefi
+            url: "https://github.com/AmaniTechnologiesLtd/Public-IOS-SDK/blob/main/Carthage/AmaniSDK/v3.3.8/AmaniSDK.xcframework.zip?raw=true",
+            checksum: "f4f05590fd9b513967a0ec2cb82dd145bfd4b40f3e4f3215f15b007f03ceb934"
+        )
     ]
 )
